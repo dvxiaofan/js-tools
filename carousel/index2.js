d2e648c2bf8607428ef00ca6884cde4f2865eeeb
xiaofan <1353815633@qq.com>
2019-07-26 08:26:12 +0800

    实现了两个动画效果的选择

--- carousel/index2.js
+++ carousel/index2.js
@@ -58,18 +58,93 @@ class Carousel {
 
     // 设置当前显示的图片
     setShowImg(toIndex, fromIndex) {
+        // 执行动画, 执行完成后最终效果
+        // 如果没有传递动画, 则直接执行结果
         this.animation(this.panels[toIndex], this.panels[fromIndex], () => {
             this.panels.forEach(panel => panel.style.zIndex = 1);
             this.panels[toIndex].style.zIndex = 10;
         })
     }
+
+    setAnimation(animation) {
+        this.animation = animation;
+    }
 }
 
-// 处理动画方式
-function fade(fromNode, toNode, callback) {
-    console.log(fromNode, toNode);
+// // 处理动画方式
+// function fade(fromNode, toNode, callback) {
+//     console.log(fromNode, toNode);
 
-    callback();
-}
+//     callback();
+// }
 
-document.querySelectorAll('.carousel').forEach(carousel => new Carousel(carousel, fade));
+const Animation = (function() {
+    const css = (node, styles) => Object.entries(styles)
+        .forEach(([key, value]) => node.style[key] = value);
+    const reset = node => node.style = '';
+
+    return {
+        // 参数使用了默认值
+        fade(during = 400) {
+            return function(to, from, onFinish) {
+                console.log(during);
+                css(from, {
+                    opacity: 1,
+                    zIndex: 10
+                })
+                css(to, {
+                    opacity: 0,
+                    zIndex: 9
+                })
+
+                setTimeout(() => {
+                    css(from, {
+                        opacity: 0,
+                        transition: `all ${during/1000}s`
+                    })
+                    css(to, {
+                        opacity: 1,
+                        transition: `all ${during/1000}s`
+                    })
+                }, 100);
+
+                setTimeout(() => {
+                    reset(from);
+                    reset(to);
+                    onFinish && onFinish();
+                }, during);
+            }
+        },
+
+        // 参数使用了默认值
+        zoom(scale = 5, during = 1000) {
+            return function(to, from, onFinish) {
+                css(from, {
+                    opacity: 1,
+                    transform: `scale(1)`,
+                    transition: `all ${during/1000}s`,
+                    zIndex: 10
+                })
+
+                css(to, {
+                    zIndex: 9
+                })
+
+                setTimeout(() => {
+                    css(from, {
+                        opacity: 0,
+                        transform: `scale(${scale})`
+                    })
+                }, 100);
+
+                setTimeout(() => {
+                    reset(from);
+                    reset(to);
+                    onFinish && onFinish();
+                }, during);
+            }
+        }
+    }
+})()
+
+const carousel = new Carousel(document.querySelector('.carousel'), Animation.zoom());
